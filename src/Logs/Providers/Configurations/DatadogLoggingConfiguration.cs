336b10b2798c633e4a4b2372c6378a0dfc3fd08f
George Zubrienko <GZU@ecco.com>
2024-03-26 16:37:06 +0100

    Fix logs exclusion

---
+++ src/Logs/Providers/Configurations/DatadogLoggingConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Serilog;
+using Serilog.Sinks.Datadog.Logs;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+namespace Snd.Sdk.Logs.Providers.Configurations
+{
+    /// <summary>
+    /// Methods to load Datadog configuration.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class DatadogLoggingConfiguration
+    {
+        private const string proteusDatadogApiKey = "PROTEUS__DATADOG_API_KEY";
+        private const string proteusDatadogSite = "PROTEUS__DATADOG_SITE";
+
+        private static string GetApiKey()
+        {
+            return Environment.GetEnvironmentVariable(proteusDatadogApiKey);
+        }
+
+        private static DatadogConfiguration CreateDefault()
+        {
+            return new DatadogConfiguration
+            {
+                Url = Environment.GetEnvironmentVariable(proteusDatadogSite)
+            };
+        }
+
+        private static bool IsDatadogEnabled()
+        {
+            return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(proteusDatadogApiKey));
+        }
+
+        /// <summary>
+        /// Adds datadog provider to serilog logging configuration
+        /// </summary>
+        /// <param name="baseConfiguration">Configuration of logger</param>
+        /// <returns></returns>
+        public static LoggerConfiguration AddDatadog(this LoggerConfiguration baseConfiguration)
+        {
+            if (IsDatadogEnabled())
+            {
+                return baseConfiguration.WriteTo.DatadogLogs(
+                    host: Dns.GetHostName(),
+                    apiKey: GetApiKey(),
+                    configuration: CreateDefault());
+            }
+            return baseConfiguration;
+        }
+    }
+}
---
+++ src/Logs/Providers/Configurations/DefaultLoggingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace Snd.Sdk.Logs.Providers.Configurations
+{
+    /// <summary>
+    /// Extension methods for configuration of all sinks
+    /// </summary>
+    public static class DefaultLoggingConfiguration
+    {
+        /// <summary>
+        /// Crates a default logging configuration
+        /// </summary>
+        /// <param name="loggerConfiguration">Serilog's configuration class</param>
+        /// <returns></returns>
+        public static LoggerConfiguration Default(this LoggerConfiguration loggerConfiguration)
+        {
+            return loggerConfiguration.WriteTo.Console();
+        }
+    }
+}
