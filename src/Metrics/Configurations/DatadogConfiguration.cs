96e6845546d6f99879d6c16e089c0962eb86eb62
George Zubrienko <GZU@ecco.com>
2024-04-22 14:16:03 +0200

    Make it look nice

--- src/Metrics/Configurations/DatadogConfiguration.cs
+++ src/Metrics/Configurations/DatadogConfiguration.cs
@@ -44,7 +44,7 @@ namespace Snd.Sdk.Metrics.Configurations
         {
             var defaultConf = new StatsdConfig
             {
-                StatsdServerName = EnvironmentExtensions.GetDomainEnvironmentVariable("DD_UNIX_DOMAIN_SOCKET_PATH") ?? "unix:///var/run/datadog/dsd.socket",
+                StatsdServerName = EnvironmentExtensions.GetDomainEnvironmentVariable("DD_UNIX_DOMAIN_SOCKET_PATH", "unix:///var/run/datadog/dsd.socket"),
                 Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
                 ServiceVersion = Environment.GetEnvironmentVariable("APPLICATION_VERSION"),
                 Prefix = metricNamespace.ToLower()
