1888afd6df16cc9eee323716e73f86291efd1c55
adelinag08 <adma@ecco.com>
2024-05-28 13:26:01 +0200

    docstrings

--- src/Storage/Providers/Configurations/RedisConfiguration.cs
+++ src/Storage/Providers/Configurations/RedisConfiguration.cs
@@ -1,12 +1,33 @@
 using Snd.Sdk.Hosting;
 
 namespace Snd.Sdk.Storage.Providers.Configurations;
-
+/// <summary>
+/// Configuration settings for connecting to a Redis instance.
+/// </summary>
 public class RedisConfiguration
 {
+    /// <summary>
+    ///  Redis server host.
+    /// </summary>
     public string Host { get; set; }
+
+    /// <summary>
+    ///  Redis server port.
+    /// </summary>
     public int Port { get; set; }
+
+    /// <summary>
+    /// Redis database number.
+    /// </summary>
     public int DatabaseNumber { get; set; }
-    public string Password => EnvironmentExtensions.GetDomainEnvironmentVariable("CACHE_REDIS_PASSWORD");
+
+    /// <summary>
+    /// Redis server connection password.
+    /// </summary>
+    public string Password => EnvironmentExtensions.GetDomainEnvironmentVariable("REDIS_CACHE_PASSWORD");i
+
+    /// <summary>
+    /// Enable SSL connectivity.
+    /// </summary>
     public bool UseSsl { get; set; }
 }
