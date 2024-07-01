eaa4f7eef82189f83267d29f1b541b9e5a726f24
Adelina Mahu <33446703+adelinag08@users.noreply.github.com>
2024-07-01 10:38:13 +0200

    Apply suggestions from code review
    Co-authored-by: Vitalii Savitskii <visa@ecco.com>

--- src/SnD.Sdk.Storage.Minio/MinioService.cs
+++ src/SnD.Sdk.Storage.Minio/MinioService.cs
@@ -21,14 +21,14 @@ public class MinioService : IMinioService
 
     public Task SetRedisBucketNotificationAsync(string bucketName, string redisQueueArn, List<BucketEvent> events)
     {
-        BucketNotification notification = new BucketNotification();
-        List<EventType> eventTypes = events.Select(e => e.ToEventType()).ToList();
+        var notification = new BucketNotification();
+        var eventTypes = events.Select(e => e.ToEventType()).ToList();
 
-        QueueConfig queueConfiguration = new QueueConfig(redisQueueArn);
+        var queueConfiguration = new QueueConfig(redisQueueArn);
         queueConfiguration.AddEvents(eventTypes);
         notification.AddQueue(queueConfiguration);
 
-        SetBucketNotificationsArgs args = new SetBucketNotificationsArgs().WithBucket(bucketName)
+        var args = new SetBucketNotificationsArgs().WithBucket(bucketName)
             .WithBucketNotificationConfiguration(notification);
         return minioClient.SetBucketNotificationsAsync(args);
     }
