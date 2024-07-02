fcc888ee80325fe4b760eb1c1e72337f6b7979df
adelinag08 <adma@ecco.com>
2024-07-02 10:53:26 +0200

    more docstrings

--- src/SnD.Sdk.Storage.Minio/Providers/MinioStorageProvider.cs
+++ src/SnD.Sdk.Storage.Minio/Providers/MinioStorageProvider.cs
@@ -5,8 +5,17 @@ using Snd.Sdk.Storage.Minio.Providers.Configurations;
 
 namespace Snd.Sdk.Storage.Minio.Providers;
 
+/// <summary>
+/// Provides extension methods to the <see cref="IServiceCollection"/> for configuring Minio storage services.
+/// </summary>
 public static class MinioStorageProvider
 {
+    /// <summary>
+    /// Adds and configures the Minio storage services.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="minioConfiguration">The configuration for the Minio storage services.</param>
+    /// <returns>The same service collection so that multiple calls can be chained.</returns>
     public static IServiceCollection AddMinioStorage(this IServiceCollection services,
         MinioConfiguration minioConfiguration)
     {
