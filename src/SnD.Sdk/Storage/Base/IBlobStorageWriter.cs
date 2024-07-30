6ae84ae51b5812cab99abd224679c35455689104
Vitalii Savitskii <visa@ecco.com>
2024-07-30 13:58:04 +0200

    Add Typed methods for the blob storage interfaces (#104)
    * Do not remove duplicates in Amazon S3 Path
    
    Part of https://github.com/SneaksAndData/arcane-stream-blob-storage/issues/41
    
    ## This PR implemented the following:
    - Path normalization introduced in #96
    - Rolled back changes in AdlsGen2Path.cs
    
    ## Additional changes
    - Updated SnD.Sdk.Tests.csproj to dotnet 8.0 since I am not able to run .net 6.0 apps on Mac OS for some reason, I don't want to investigate.
    
    * Implement typed blob storage abstractions
    
    * Fix in S3 Path construction
    
    * Add one more constructor for AmazonS3StoragePath
    
    * Paths refactoring
    
    * Refactoring finalization
    
    * Fix the return type for the join method
    
    * Fix formatting issues

--- src/SnD.Sdk/Storage/Base/IBlobStorageWriter.cs
+++ src/SnD.Sdk/Storage/Base/IBlobStorageWriter.cs
@@ -1,47 +1,44 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Snd.Sdk.Storage.Models;
+using Snd.Sdk.Storage.Models.Base;
 
 namespace Snd.Sdk.Storage.Base;
 
 /// <summary>
 /// Binary object storage write-only abstraction.
 /// </summary>
-public interface IBlobStorageWriter
+public interface IBlobStorageWriter<in TBlobPath> where TBlobPath : IStoragePath
 {
     /// <summary>
     /// Saves byte array to a blob.
     /// </summary>
     /// <param name="bytes">Bytes to save.</param>
     /// <param name="blobPath">Blob path.</param>
-    /// <param name="blobName">Blob name.</param>
     /// <param name="overwrite">Whether to overwrite a blob or fail if it exists.</param> 
     /// <returns></returns>
-    Task<UploadedBlob> SaveBytesAsBlob(BinaryData bytes, string blobPath, string blobName, bool overwrite = false);
+    Task<UploadedBlob> SaveBytesAsBlob(BinaryData bytes, TBlobPath blobPath, bool overwrite = false);
 
     /// <summary>
     /// Saves string data of a moderate size to a blob.
     /// </summary>
     /// <param name="text">Blob contents.</param>
     /// <param name="blobPath">Blob path.</param>
-    /// <param name="blobName">Blob name.</param>
     /// <returns></returns>
-    Task<UploadedBlob> SaveTextAsBlob(string text, string blobPath, string blobName);
+    Task<UploadedBlob> SaveTextAsBlob(string text, TBlobPath blobPath);
 
     /// <summary>
     /// Deletes a blob.
     /// </summary>
     /// <param name="blobPath">Blob path.</param>
-    /// <param name="blobName">Blob name.</param>
     /// <returns></returns>
-    Task<bool> RemoveBlob(string blobPath, string blobName);
+    Task<bool> RemoveBlob(TBlobPath blobPath);
 
     /// <summary>
     /// Generates a pre-signed URL that can be used to read the blob over HTTP(S) protocol.
     /// </summary>
-    /// <param name="blobPath">Path to the blob.</param>
-    /// <param name="blobName">Name of the blob.</param>
+    /// <param name="path">Path to the blob.</param>
     /// <param name="kwOptions">Additional key-value arguments for URI generator.</param>
     /// <returns></returns>
-    Uri GetBlobUri(string blobPath, string blobName, params ValueTuple<string, object>[] kwOptions);
+    Uri GetBlobUri(TBlobPath path, params ValueTuple<string, object>[] kwOptions);
 }
--- test/Storage/AldsGen2PathTests.cs
+++ test/Storage/AldsGen2PathTests.cs
@@ -16,7 +16,7 @@ public class AdlsGen2PathTests
 
         // Assert
         Assert.Equal("container", storagePath.Container);
-        Assert.Equal("folder1/folder2/file.txt", storagePath.ObjectKey);
+        Assert.Equal("folder1/folder2/file.txt", storagePath.FullPath);
     }
 
     [Fact]
@@ -27,32 +27,5 @@ public class AdlsGen2PathTests
 
         // Assert
         Assert.Equal(path, storagePath.ToHdfsPath());
-    }
-
-    [Fact]
-    public static void CanJoinPath()
-    {
-        // Arrange
-        const string pathStart = "abfss://container@/";
-
-        // Act
-        var storagePath = new AdlsGen2Path(pathStart).Join("folder1/folder2/file.txt");
-
-        // Assert
-        Assert.Equal(path, storagePath.ToHdfsPath());
-    }
-
-    [Fact]
-    public static void TrimsDuplicatedSlashes()
-    {
-        // Arrange
-        const string pathStart = "abfss://container@/";
-        const string expected = "abfss://container@folder1///folder2/file.txt";
-
-        // Act
-        var storagePath = new AdlsGen2Path(pathStart).Join("/folder1///folder2/file.txt");
-
-        // Assert
-        Assert.Equal(expected, storagePath.ToHdfsPath());
     }
 }
