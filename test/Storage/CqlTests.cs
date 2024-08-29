658bfed13e7d569cd50f95e830c44feaf4799ec2
adelinag08 <adma@ecco.com>
2024-08-29 13:17:28 +0300

    fix

--- test/Storage/CqlTests.cs
+++ test/Storage/CqlTests.cs
@@ -6,40 +6,42 @@ using Moq;
 using Snd.Sdk.Storage.Cql;
 using Xunit;
 
-namespace Snd.Sdk.Tests.Storage;
-
-public class CqlTests: IClassFixture<AkkaFixture>, IClassFixture<LoggerFixture>
+namespace Snd.Sdk.Tests.Storage
 {
-    private readonly AkkaFixture akkaFixture;
-    private readonly LoggerFixture loggerFixture;
-    public CqlTests(AkkaFixture akkaFixture, LoggerFixture loggerFixture)
+    public class CqlTests : IClassFixture<AkkaFixture>, IClassFixture<LoggerFixture>
     {
-        this.akkaFixture = akkaFixture;
-        this.loggerFixture = loggerFixture;
-    }
+        private readonly AkkaFixture akkaFixture;
+        private readonly LoggerFixture loggerFixture;
 
-    [Theory]
-    [InlineData("1000 per second", true)]
-    [InlineData("50000 per minute", true)]
-    public async Task ExecuteWithRetryAndRateLimit_ExecutesSuccessfully(string rateLimit, bool expectedResult)
-    {
-        var loggerMock = new Mock<ILogger<object>>();
-        var cqlApiCallMock = new Mock<Func<CancellationToken, Task<bool>>>();
-        cqlApiCallMock.Setup(c => c(It.IsAny<CancellationToken>())).ReturnsAsync(expectedResult);
-        var cancellationToken = CancellationToken.None;
+        public CqlTests(AkkaFixture akkaFixture, LoggerFixture loggerFixture)
+        {
+            this.akkaFixture = akkaFixture;
+            this.loggerFixture = loggerFixture;
+        }
 
-        var result = await cqlApiCallMock.Object.ExecuteWithRetryAndRateLimit(loggerMock.Object, rateLimit, cancellationToken);
+        [Theory]
+        [InlineData("1000 per second", true)]
+        [InlineData("50000 per minute", true)]
+        public async Task ExecuteWithRetryAndRateLimit_ExecutesSuccessfully(string rateLimit, bool expectedResult)
+        {
+            var loggerMock = new Mock<ILogger<object>>();
+            var cqlApiCallMock = new Mock<Func<CancellationToken, Task<bool>>>();
+            cqlApiCallMock.Setup(c => c(It.IsAny<CancellationToken>())).ReturnsAsync(expectedResult);
+            var cancellationToken = CancellationToken.None;
 
-        Assert.True(result);
-    }
+            var result = await cqlApiCallMock.Object.ExecuteWithRetryAndRateLimit(loggerMock.Object, rateLimit, cancellationToken);
 
-    [Fact]
-    public async Task ExecuteWithRetryAndRateLimit_InvalidRateLimitUnit()
-    {
-        var loggerMock = new Mock<ILogger<object>>();
-        var cqlApiCallMock = new Mock<Func<CancellationToken, Task<int>>>();
-        var cancellationToken = CancellationToken.None;
+            Assert.True(result);
+        }
 
-        await Assert.ThrowsAsync<ArgumentException>(() => cqlApiCallMock.Object.ExecuteWithRetryAndRateLimit(loggerMock.Object, "1000 per min", cancellationToken));
+        [Fact]
+        public async Task ExecuteWithRetryAndRateLimit_InvalidRateLimitUnit()
+        {
+            var loggerMock = new Mock<ILogger<object>>();
+            var cqlApiCallMock = new Mock<Func<CancellationToken, Task<int>>>();
+            var cancellationToken = CancellationToken.None;
+
+            await Assert.ThrowsAsync<ArgumentException>(() => cqlApiCallMock.Object.ExecuteWithRetryAndRateLimit(loggerMock.Object, "1000 per min", cancellationToken));
+        }
     }
 }
